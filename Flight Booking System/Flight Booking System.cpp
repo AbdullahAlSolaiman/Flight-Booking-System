f9c93c65c6bbb3230ed00e8be6cf1139b87879ad
Tareq Al-Ahdal <Tareqx1999@gmail.com>
2019-10-28 13:19:44 +0800

    Creating the project

---
+++ Flight Booking System/Flight Booking System.cpp
@@ -0,0 +1,11 @@
+﻿// Flight Booking System.cpp : Defines the entry point for the application.
+//
+
+#include "Flight Booking System.h"
+
+using namespace std;
+
+int main() {
+	cout << "Hello CMake." << endl;
+	return 0;
+}
