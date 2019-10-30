33d7050d2a1d9542ac4a17441136335e4adddbcd
Tareq Al-Ahdal <Tareqx1999@gmail.com>
2019-10-30 17:17:57 +0800

    Added Attributes to class Flight

--- Flight Booking System/Flight.cpp
+++ Flight Booking System/Flight.cpp
@@ -1,15 +1,50 @@
 // Implementation file for the Flight class
 #include <iostream>
 #include "Flight.h"
+using std::cout;
+using std::endl;
+using std::string;
 
 Flight::Flight() {
-	std::cout << "Flight has been created" << std::endl;
+	_Price = _Duration = 0.0;
+}
+
+void Flight::SetPrice(float amount) {
+	_Price = amount;
+}
+
+float Flight::GetPrice() {
+	return _Price;
+}
+
+void Flight::SetArrivalTime(std::string time) {
+	_ArrivalTime = time;
+}
+
+string Flight::GetArrivalTime() {
+	return _ArrivalTime;
+}
+
+void Flight::SetDuration(float hours) {
+	_Duration = hours;
+}
+
+float Flight::GetDuration() {
+	return _Duration;
+}
+
+void Flight::SetPlane(Plane plane) {
+	_FlightPlane = &plane;
+}
+
+Plane Flight::GetPlane() {
+	return *_FlightPlane;
 }
 
 Flight::~Flight() {
-	std::cout << "Flight has been destroyed" << std::endl;
+	cout << "Flight has been destroyed" << endl;
 }
 
 void Flight::test() {
-	std::cout << "Testing Flight" << std::endl;
+	cout << "Testing Flight" << endl;
 }
--- Flight Booking System/Flight.h
+++ Flight Booking System/Flight.h
@@ -2,12 +2,30 @@
 #ifndef FLIGHT_H
 #define FLIGHT_H
 
+#include "Plane.h"
+#include <string>
+using std::string;
+
+
 class Flight {
-private:
-public:
-	Flight();
-	~Flight();
-	void test();
+	private:
+		float _Price;
+		float _Duration;
+		string _DepartureTime;
+		string _ArrivalTime;
+		Plane* _FlightPlane;
+	public:
+		Flight();
+		void SetPrice(float);
+		float GetPrice();
+		void SetArrivalTime(string);
+		string GetArrivalTime();
+		void SetDuration(float);
+		float GetDuration();
+		void SetPlane(Plane);
+		Plane GetPlane();
+		~Flight();
+		void test();
 };
 
 #endif
